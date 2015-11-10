4156f9531b5e45891a41ba9661c361690aed77db
prmths128 <plloret@gmx.com>
2015-11-10 21:32:19 +0100

    Replaced Ward clustering with AgglomerativeClustering for sklearn 0.17.

--- bakfu/classify/cluster/ward_sklearn.py
+++ bakfu/classify/cluster/ward_sklearn.py
@@ -4,7 +4,8 @@
 Ward clustering using sklearn
 '''
 
-from sklearn.cluster import Ward
+from sklearn.cluster import AgglomerativeClustering
+
 from sklearn import metrics
 from sklearn.preprocessing import StandardScaler
 
@@ -16,32 +17,4 @@ from .base import BaseClusterizer
 class WardClusterizer(BaseClusterizer):
 
     init_kwargs = ('n_clusters',)
-    classifier_class = Ward
-
-    #def __init__(self, *args, **kwargs):
-        #super(WardClusterizer, self).__init__(*args, **kwargs)
-
-        #self.clusterizer = Ward(**kwargs)
-
-    #def clusterize(self, data_source, *args, **kwargs):
-        #'''
-
-        #'''
-
-        #self.results = {'clusters':result}
-
-        #return result
-
-    #def run(self, caller, *args, **kwargs):
-        #data_source = caller.data['main_data']
-        #vectorizer_result = caller.data['vectorizer_result']
-
-        #X = vectorizer_result
-        #predictions = self.clusterizer.fit_predict(X.toarray())
-
-        #caller.data['clusterizer_result'] = predictions
-        #caller.data['clusterizer'] = self.clusterizer
-
-        #return self.update(
-            #result=predictions,
-            #clusterizer_result=predictions)
+    classifier_class = AgglomerativeClustering
